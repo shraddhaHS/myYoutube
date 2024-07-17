0e9658399dbc694387ec491c04b8dfa7de77ba7a
shraddha1dir@gmail.com <shraddha1dir@gmail.com>
2024-07-17 15:20:27 +0530

    fixes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,17 +3,18 @@ class ApiError extends Error{
         statusCode,
         message="something went wrong",
         errors=[],
-        statck = ""
+        stack = ""
 
     ){
         super(message)
         this.statusCode = statusCode
+        this.message = message
         this.data = null
         this.success = false
         this.error = errors
 
-        if(statck){
-            this.stack= statck
+        if(stack){
+            this.stack= stack
         }else{
             Error.captureStackTrace(this,this.constructor)
         }
